7c6ac6ef2b720fbc3ae9abd7e923ebd89b5c541a
sunwooKim <frod5@kakao.com>
2023-04-30 17:34:42 +0900

    스프링 부트 - 핵심 원리와 활용 : 외부설정 - 정리

--- src/main/java/hello/EnvironmentCheck.java
+++ src/main/java/hello/EnvironmentCheck.java
@@ -35,6 +35,20 @@ public class EnvironmentCheck {
         //우선순위는 상식 선에서 딱 2가지만 기억하면 된다.
         //더 유연한 것이 우선권을 가진다. (변경하기 어려운 파일 보다 실행시 원하는 값을 줄 수 있는 자바 시스템 속성이 더 우선권을 가진다.)
         //범위가 넒은 것 보다 좁은 것이 우선권을 가진다. (자바 시스템 속성은 해당 JVM 안에서 모두 접근할 수 있다. 반면에 커맨드 라인 옵션 인수는 main 의 arg를 통해서 들어오기 때문에 접근 범위가 더 좁다.)
+
+
+        //우선순위
+        //@TestPropertySource (테스트에서 사용)
+        //커맨드 라인 옵션 인수
+        //자바 시스템 속성
+        //OS 환경변수
+        //설정 데이터( application.properties )
+
+        //설정 데이터(application.properties) 우선순위. 프로젝트 안에 있는 application.properties가 내부.
+        //jar 외부 프로필 적용 파일 application-{profile}.properties
+        //jar 외부 application.properties
+        //jar 내부 프로필 적용 파일 application-{profile}.properties
+        //jar 내부 application.properties
         String url = environment.getProperty("url");
         String username = environment.getProperty("username");
         String password = environment.getProperty("password");
